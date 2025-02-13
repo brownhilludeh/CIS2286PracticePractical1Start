0c71dd68e0a9e4b0946dc9444fb24ea1d2eb1be9
brownhilludeh <brownhilludeh@gmail.com>
2025-02-13 17:28:05 -0400

    version 2

--- CIS2286PracticePractical1.js
+++ CIS2286PracticePractical1.js
@@ -1,6 +1,6 @@
 /**
  *
- * @author jdkitson
+ * @author Brownhill Udeh
  */
 
 //Declare local variables/attributes
@@ -20,7 +20,8 @@ let phoneCountTwo = 0;
 let phoneCountThree = 0;
 
 let budget = 1000;
-let phonesPurchased = 0;
+//let phonesPurchased = "";
+let payable = 0;
 let numPhones = 0;
 let subTotal = 0;
 let purchaserName;
@@ -52,18 +53,10 @@ function phoneSalesApp() {
     let option = "";
 
     //Capture purchaser name and budget
-    purchaserName = prompt("What is your name?");
-    budget = prompt("What is your budget?");
+
 
     do {
         option = prompt(MENU);
-        // while (!"PVX".includes(option.toUpperCase())) {
-        //     option = prompt("Invalid option. Please enter P to purchase a phone, V to view order details or X to exit. " + MENU);
-        // }
-
-        while (option.toUpperCase() !== "P" && option.toUpperCase() !== "V" && option.toUpperCase() !== "X") {
-            option = prompt(MENU);
-        }
         processMenuOption(option);
     } while (option.toUpperCase() !== EXIT);
 
@@ -82,19 +75,7 @@ function phoneSalesApp() {
 function processMenuOption(option) {
     //Add a switch to process the option
 
-    switch (option.toUpperCase()) {
-        case "P":
-            purchase();
-            break;
-        case "BrownV":
-            alert(displayOrder());
-            break;
-        case "X":
-            alert(displayOrder() + "Good Bye!" + "\nThank you for using CIS Cellphone Sales App");
-            break;
-        default:
-            break;
-    }
+
 }
 
 /**
@@ -102,37 +83,19 @@ function processMenuOption(option) {
  */
 function purchase() {
 
+
     const PHONE_MENU = "------------------------\n"
         + "Phone Menu\n"
-        + "I - iPhone X - $400\n"
-        + "S - Samsung Galaxy S10 - $450\n"
-        + "F - Nokia 2720 - Flip Phone - $150\n"
+        + "- I-iPhone X - $400\n"
+        + "- S-Samsung Galaxy S10 - $450\n"
+        + "- F- Nokia 2720 - Flip Phone - $150\n"
         + "-------------------------\n"
         + "Option-->";
     let phoneOption;
 
     phoneOption = prompt(PHONE_MENU);
 
-    while (phoneOption.toUpperCase() !== "I" && phoneOption.toUpperCase() !== "S" && phoneOption.toUpperCase() !== "F") {
-
-        phoneOption = prompt(PHONE_MENU + "Invalid option. Please enter I to purchase an iPhone X, S to purchase a Samsung Galaxy S10 or F to purchase a Nokia 2720. ");
-    }
-
     //Process purchase (maybe a Switch?)
-    switch (phoneOption.toUpperCase()) {
-        case "I":
-            processPhone(PHONE_TYPE_1, PHONE_ONE_PRICE);
-            break;
-        case "S":
-            processPhone(PHONE_TYPE_2, PHONE_TWO_PRICE);
-            break;
-        case "F":
-            processPhone(PHONE_TYPE_3, PHONE_THREE_PRICE);
-            break;
-        default:
-            break;
-    }
-
 
 
 }
@@ -147,35 +110,14 @@ function purchase() {
  */
 function processPhone(phoneType, phoneCost) {
     //Verify there is ample budget for purchase - phone cost including Tax
-    if (budget < phoneCost + (phoneCost * TAX_RATE)) {
-        alert("Insufficient funds. Your current budget is " + formatCurrency(budget));
-    } else {
-        //If budget remains, updates total number phones purchased ,individual phone type totals
-        numPhones++;
-        subTotal += phoneCost;
-        switch (phoneType) {
-            case "iPhone X":
-                phoneCountOne++;
-                break;
-            case "Samsung Galaxy S10":
-                phoneCountTwo++;
-                break;
-            case "Nokia 2720":
-                phoneCountThree++;
-                break;
-            default:
-                break;
-        }
 
-        //adjust budget amount after a successful purchase transaction
-        budget -= phoneCost + (phoneCost * TAX_RATE);
+    //If budget remains, updates total number phones purchased ,individual phone type totals
 
-        //Prints process phone feedback e.g.You purchased an iPhone X at a cost of $460.00. Your new budget is $540.00 or
-        // Insufficient funds. Your current budget is $80.00.
-        alert("You purchased a " + phoneType + " at a cost of " + formatCurrency(phoneCost + (phoneCost * TAX_RATE)) + ". Your new budget is " + formatCurrency(budget));
 
-    }
+    //adjust budget amount after a successful purchase transaction
 
+    //Prints process phone feedback e.g.You purchased an iPhone X at a cost of $460.00. Your new budget is $540.00 or
+    // Insufficient funds. Your current budget is $80.00.
 
 
 }
@@ -200,15 +142,7 @@ function formatCurrency(amount, hasSeparator = true) {
  */
 function displayOrder() {
 
-    return "Purchaser Name: " + purchaserName
-        + "\n Number of Items: 3" + numPhones
-        + "\n Purchased: "
-        + "\n " + phoneCountOne + " iPhone X(s), "
-        + "\n " + phoneCountTwo + " Samsung Galaxy S10(s), "
-        + "\n " + phoneCountThree + " Nokia 2720(s) at a cost of $400.00, $450.00, $150.00 each\n" +
-        + "\n Subtotal: " + formatCurrency(subTotal)
-        + "\n Tax: " + formatCurrency(subTotal * TAX_RATE)
-        + "\n Total: " + formatCurrency(subTotal + subTotal * TAX_RATE);
+    return "";
 }
 
 
